549fcfb63c6cdf9eab0d0fd7bd789b72b373863c
Ryan P Kilby <rpkilby@ncsu.edu>
2013-04-29 14:01:35 -0300

    Update admin.py

--- emailuser/admin.py
+++ emailuser/admin.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import ugettext_lazy as _
@@ -10,7 +9,7 @@ from models import EmailUser
 class EmailUserAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
